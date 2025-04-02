154764f43a30b8c0edd204f081b8d6424508f3de
Sebastian Westerholm <westerholm.sebastian@gmail.com>
2025-04-02 13:32:43 +0200

    Some more work done on component for updating dog info

--- frontend/src/components/UpdateDog/UpdateDog.jsx
+++ frontend/src/components/UpdateDog/UpdateDog.jsx
@@ -1,31 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import "./Update.css";
 
 function UpdateDog() {
   const location = useLocation();
   const thisDog = location.state;
+  const [newDogInfo, setNewDogInfo] = useState({
+    changedName: `${thisDog.name}`,
+    changedAge: `${thisDog.age}`,
+    changedBio: `${thisDog.bio}`,
+  });
+
+  // console.log(newDogInfo);
 
   //   On submit handler!
+  function submitHandler(e) {
+    e.preventDefault();
+    console.log(thisDog);
+    console.log(newDogInfo);
+  }
+
+  function handleChange(e) {
+    const { id, value } = e.target;
+    setNewDogInfo((prevInfo) => ({
+      ...prevInfo,
+      [id]: value,
+    }));
+  }
 
   return (
     <div className="update-dog-info">
       <h1>Update dog info</h1>
-      <form action="">
+      <form action="" onSubmit={submitHandler}>
         {/* Update name */}
-        <label htmlFor="change-name">New name: </label>
-        <input type="text" placeholder={`${thisDog.name}`} id="change-name" />
+        <label htmlFor="changedName">New name: </label>
+        <input
+          type="text"
+          placeholder={`${thisDog.name}`}
+          id="changedName"
+          onChange={handleChange}
+        />
 
         {/* Update age */}
-        <label htmlFor="change-age">Change age: </label>
-        <input type="number" placeholder={`${thisDog.age}`} min={0} max={20} />
+        <label htmlFor="changedAge">Change age: </label>
+        <input
+          type="number"
+          id="changedAge"
+          placeholder={`${thisDog.age}`}
+          min={0}
+          max={20}
+          onChange={handleChange}
+        />
 
         {/* Update bio */}
-        <label htmlFor="change-bio">Change bio: </label>
+        <label htmlFor="changedBio">Change bio: </label>
         <textarea
-          id="change-bio"
+          id="changedBio"
           maxLength="150"
           placeholder={`${thisDog.bio}`}
+          onChange={handleChange}
         ></textarea>
 
         <input type="submit" value="Save" />
