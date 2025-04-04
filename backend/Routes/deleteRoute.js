7d97dd0950c1b2ed7f3c1c54a2246c477470a8f1
Sebastian Westerholm <westerholm.sebastian@gmail.com>
2025-04-04 14:13:11 +0200

    Delete dog from database and remove profile image from uploads

--- backend/Routes/deleteRoute.js
+++ backend/Routes/deleteRoute.js
@@ -1,12 +1,29 @@
 import express from "express";
 import { dogs } from "../models/dogSchema.js";
+import fs from "fs/promises";
+
 const deleteRouter = express.Router();
 
 deleteRouter.delete("/", async (req, res) => {
   try {
     const id = req.body.id;
-    await dogs.deleteOne({ _id: id });
+    const thisDog = await dogs.findById({ _id: id });
+    const imgPath = `./${thisDog.profilePic}`;
+    console.log(imgPath);
 
+    // Delete profile pic if not null
+    if (thisDog.profilePic !== null) {
+      console.log("Not null");
+      try {
+        await fs.unlink(imgPath);
+        console.log("Deleted image:", imgPath);
+      } catch (err) {
+        console.warn("Could not delete image from ./uploads");
+      }
+    }
+
+    // Delete dog from database
+    await dogs.deleteOne({ _id: id });
     res.status(200).json({ status: `Dog deleted: ${id}` });
   } catch (err) {
     console.error("Could not delete dog:", err);
