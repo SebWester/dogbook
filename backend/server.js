3d5ab0314f2770dc07ade5c2197f0fbf473a6a4b
Sebastian Westerholm <westerholm.sebastian@gmail.com>
2025-04-10 17:42:51 +0200

    Add Helmet middleware for basic security headers and XSS protection

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
 import mongoDB from "./database.js";
 
 const app = express();
@@ -10,6 +11,7 @@ mongoDB();
 app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
 app.use(express.static("uploads"));
+app.use(helmet());
 
 // Routes
 import createRouter from "./Routes/createRouter.js";
